2eed6786ab757e5bdf8b5b256adfb740a0a88817
Wawa <36123831+ketnas@users.noreply.github.com>
2021-01-04 11:30:25 +0700

    Update model.py

--- lib/model.py
+++ lib/model.py
@@ -22,9 +22,9 @@ import keras.layers as KL
 import keras.engine as KE
 import keras.models as KM
 
-#from lib import utils
+from Deepfashion2_Training.lib import utils
 
-import utils
+#import utils
 
 # Requires TensorFlow 1.3+ and Keras 2.0.8+.
 from distutils.version import LooseVersion
